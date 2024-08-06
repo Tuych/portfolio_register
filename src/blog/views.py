67b7f5b32c0dedac79642c3388421a3d9ece7fa9
Tuych <tuychiyevbaxtiyor06@gmail.com>
2024-08-06 15:34:33 +0500

    added decorator function - login_required

--- src/blog/views.py
+++ src/blog/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Blog
+from django.contrib.auth.decorators import login_required
 
 
+@login_required()
 def blogs(request):
     blogs = Blog.objects.order_by('-date')  # data ni usish tartibida chiqaradi 2-yul, 5-iyul
     return render(request, 'blog/blogs.html', {'blogs': blogs})
 
 
+@login_required()
 def detail(request, blog_id):
     blog = get_object_or_404(Blog, pk=blog_id)
     return render(request, 'blog/details.html', {'blog': blog})
--- src/skills/views.py
+++ src/skills/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from .models import Skills
+from django.contrib.auth.decorators import login_required
 
 
+@login_required()
 def index(request):
     projects = Skills.objects.all()
     print(projects)
